03ba1372ecd305c9e3afe5c61ce825b1f4919512
Fatima Masood <fatimamughal103@gmail.com>
2026-06-27 23:36:21 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,3 @@
+# this is cli based code rather than gui based code, add ai features and much more InshaAllah..
+# code is written in python 3.11.4 and tested on windows 11
+# This program is not using any ai tools or any other tools, this is purely written by Fatima Masood because this is for learning/practice purposes..
