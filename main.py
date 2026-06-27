a99a03bae2b7b08c3b3ec316d20468d285c06abd
Fatima Masood <fatimamughal103@gmail.com>
2026-06-28 01:11:21 +0500

    Add initial project files and implement main menu functionality

--- main.py
+++ main.py
@@ -1,3 +1,44 @@
-# this is cli based code rather than gui based code, add ai features and much more InshaAllah..
-# code is written in python 3.11.4 and tested on windows 11
-# This program is not using any ai tools or any other tools, this is purely written by Fatima Masood because this is for learning/practice purposes..
+
+def show_menu():
+
+    print("You can select an option according to your need:")
+    print("1: Upload Notes")
+    print("2: View Notes")
+    print("3: Search Notes")
+   # print("4: Save Answers")
+   # print("5: View Saved Answers")
+    print("4: Exit")
+
+def main():
+    print("==============================")
+    print("WELCOME TO THE STUDYPILOT CLI")
+    print("==============================")
+
+    show_menu()
+
+    choice = input("Enter your choice (1-4): ")
+
+    while choice != '4':
+        if choice == '1':
+            upload_notes()
+        elif choice == '2':
+            view_notes()
+        elif choice == '3':
+            search_notes()
+       # elif choice == '4':
+       #     save_answers()
+       # elif choice == '5':
+        #    view_saved_answers()
+        elif choice == '4':
+            print("Thank you for using StudyPilot CLI. Goodbye!")
+
+        else:
+            print("Invalid choice. Please choose carefully.")
+
+
+        show_menu()
+        choice = input("Enter your choice (1-4): ")
+
+
+if __name__ == "__main__":
+    main()
